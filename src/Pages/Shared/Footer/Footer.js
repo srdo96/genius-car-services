6eb9825213af4195002c1e308760317cbb542d62
Sakib Rahman <seu.sakib@gmail.com>
2022-04-11 10:26:59 +0600

    add copyright and current year in Footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <p>
+        <small>copyright &copy; {new Date().getFullYear()}</small>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
