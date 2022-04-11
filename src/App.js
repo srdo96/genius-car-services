8c644ea037e96d961a06b4008b3ddc7595b7d60f
Sakib Rahman <seu.sakib@gmail.com>
2022-04-11 12:18:29 +0600

    add fake data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route to="/" element={} />
-        <Route to="/about" element={} />
+        <Route to="/" element={<Home />} />
+        <Route to="/about" element={<About />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Services from "../../Home/Services/Services";
 
 const Header = () => {
   return (
     <header>
-      <h2>This is header</h2>
+      <Services></Services>
     </header>
   );
 };
