592bae3931d2ae4e3b6adff842418ec92760d0ca
Sakib Rahman <seu.sakib@gmail.com>
2022-04-13 22:50:51 +0600

    add RequireAuth

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
 import Login from "./Pages/Login/Login/Login";
 import Register from "./Pages/Login/Register/Register";
+import RequireAuth from "./Pages/Login/RequireAuth/RequireAuth";
+import CheckOut from "./Pages/CheckOut/CheckOut/CheckOut";
 
 function App() {
   return (
@@ -23,6 +25,14 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route
+          path="/checkout"
+          element={
+            <RequireAuth>
+              <CheckOut></CheckOut>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
   return (
-    <div>
+    <div className="text-center mt-5">
       <h2>Welcome to Detail: {serviceId}</h2>
+      <div>
+        <Link to="/checkout">
+          <button className="btn btn-primary mt-3">CheckOut</button>
+        </Link>
+      </div>
     </div>
   );
 };
