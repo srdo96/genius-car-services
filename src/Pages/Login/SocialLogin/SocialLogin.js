1aed69085ffced6818055289e9241a13158a2630
Sakib Rahman <seu.sakib@gmail.com>
2022-04-14 11:28:45 +0600

    add signin with google functionality

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,7 +2,25 @@ import React from "react";
 import google from "../../../images/social/google.svg";
 import github from "../../../images/social/github.svg";
 import facebook from "../../../images/social/facebook.svg";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+
+  let errorElement;
+  if (error) {
+    errorElement = (
+      <div>
+        <p className="text-danger">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    navigate("/home");
+  }
   return (
     <div>
       <div className="d-flex align-items-center ">
@@ -10,8 +28,12 @@ const SocialLogin = () => {
         <p className="mt-2 p-3">or</p>
         <div style={{ height: "1px" }} className="bg-primary w-50"></div>
       </div>
+      {errorElement}
       <div>
-        <button className="btn btn-info d-block mx-auto my-2 w-50">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-info d-block mx-auto my-2 w-50"
+        >
           <img className="" src={google} alt="" /> Google Sign in
         </button>
         <button className="btn btn-info d-block mx-auto my-2 w-50">
