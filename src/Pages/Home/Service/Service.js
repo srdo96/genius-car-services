75d5c909c6851e47a6902efe239c2ef8f7377c18
Sakib Rahman <seu.sakib@gmail.com>
2022-04-24 15:01:33 +0600

    fetch data from mongoDB

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "./Service.css";
 
 const Service = ({ service }) => {
-  const { id, name, img, description, price } = service;
+  const { _id, name, img, description, price } = service;
   const navigate = useNavigate();
   const navigateToServiceDetail = (id) => {
     navigate(`/service/${id}`);
@@ -17,7 +17,7 @@ const Service = ({ service }) => {
         <small>{description}</small>
       </p>
       <button
-        onClick={() => navigateToServiceDetail(id)}
+        onClick={() => navigateToServiceDetail(_id)}
         className="btn btn-primary"
       >
         Book: {name}
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
   const [services, setServices] = useState([]);
 
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
@@ -15,7 +15,7 @@ const Services = () => {
         <h1 className="services-title mt-5 mb-3">Our Services</h1>
         <div className="services-container">
           {services.map((service) => (
-            <Service key={service.id} service={service} />
+            <Service key={service._id} service={service} />
           ))}
         </div>
       </div>
