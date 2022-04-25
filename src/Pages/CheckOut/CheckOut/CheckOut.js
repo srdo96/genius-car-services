9de18dfd6d077824b760dd265e310c81f6e45d6d
Sakib Rahman <seu.sakib@gmail.com>
2022-04-25 14:23:39 +0600

    add useServiceDetail hook

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useServiceDetail from "../../../hooks/useServiceDetail";
 
 const CheckOut = () => {
+  const { serviceId } = useParams();
+  const [service] = useServiceDetail(serviceId);
   return (
-    <div>
-      <h2>Please CheckOut your Booking</h2>
+    <div className="text-center pt-5">
+      <h2>Please Order:</h2>
+      <h3>{service.name}</h3>
     </div>
   );
 };
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import useServiceDetail from "../../hooks/useServiceDetail";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
-  const [service, setService] = useState({});
-  useEffect(() => {
-    const url = `http://localhost:5000/service/${serviceId}`;
-    console.log(url);
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setService(data));
-  }, []);
+  const [service] = useServiceDetail(serviceId);
   console.log(service);
   return (
     <div className="text-center mt-5">
-      {/* <h2>Welcome to Detail: {service.name}</h2> */}
+      <h1>You are about to Book</h1>
+      <h4>{service.name}</h4>
       <img className="w-25" src={service.img} alt="" />
-      <h2>{service.name}</h2>
       <p>Price: {service.price}</p>
       <div>
-        <Link to="/checkout">
+        <Link to={`/checkout/${serviceId}`}>
           <button className="btn btn-primary mt-3">CheckOut</button>
         </Link>
       </div>
